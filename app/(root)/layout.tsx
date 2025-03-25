ed6bfe852c85e316831ed3ce3ded161d2b547c62
Abdul Rauf <141711142+Raufjatoi@users.noreply.github.com>
2025-03-25 21:56:26 +0500

    worked on the interview page
    worked on the interview page like the ai and the client added some properties and make it functional a bit ig

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,21 @@
 import Image from 'next/image';
 import {ReactNode} from 'react'
 import Link from 'next/link';
+import { isAuthenticated } from '@/lib/actions/auth.action';
+import { redirect } from 'next/navigation';
 
-const RootLayout = ({children}: {children: ReactNode}) => {
+
+const RootLayout = async ({children}: {children: ReactNode}) => {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if(!isUserAuthenticated) redirect('/sign-in');
+
   return (
     <div className='root-layout'>
       <nav>
         <Link href="/" className='flex items-center gap-2'>
-        <Image src="/logo.svg" alt="logo" width={38} height={32}>
-        </Image>
+        <Image src="/logo.svg" alt="logo" width={38} height={32} />
+
         <h2 className='text-primary-100'>Mocka</h2>
         </Link>
       </nav>
